7d4a8f82e10db49a2036365f6e18352d79ee3274
Nguyen Sy Huan <huanns@falcongames.com>
2025-06-25 08:38:30 +0700

    add player stats

---
+++ Assets/Scripts/InGame/Player/PlayerStats.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace InGame
+{
+    [CreateAssetMenu(menuName = "InGame/Player/Player Stats", fileName = "PlayerStats")]   
+    public class PlayerStats : ScriptableObject
+    {
+        public int hp; // Each tower will take this hp value
+        public float lifeLeech;
+        public int damage; // Player base damage
+        public float cooldown; // %
+        public float criticalRate; // Critical rate
+        public float criticalDamage; // Critical Damage multiplier
+    }
+}
