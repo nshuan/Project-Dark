feecb9fd2383a4b4170e7b9c01d49d2775f09376
Nguyen Sy Huan <huanns@falcongames.com>
2025-07-01 10:23:56 +0700

    add triple shot

--- Assets/Scripts/InGame/MouseInput/MoveProjectileShot.cs
+++ Assets/Scripts/InGame/MouseInput/MoveProjectileShot.cs
@@ -166,7 +166,7 @@ namespace InGame
             
             mousePosition = Input.mousePosition;
             mousePosition.z = 0; // Set z to 0 for 2D
-            cursorRect.position = mousePosition;
+            cursorRect.position = mousePosition;    
             
             // Cooldown if player can not shoot
             if (!CanShoot)
---
+++ Assets/Scripts/InGame/PlayerSkills/ShotBehaviour/PlayerShootTriple.cs
@@ -0,0 +1,39 @@
+using System;
+using InGame.Pool;
+using UnityEngine;
+
+namespace InGame
+{
+    [Serializable]
+    public class PlayerShootTriple : PlayerSkillBehaviour
+    {
+        [Range(0f, 180f)] public float angle;
+        
+        public override void Shoot(ProjectileEntity projectilePrefab, Vector2 spawnPos, float bulletRange, Vector2 target, int damagePerBullet, int criticalDamagePerBullet, float criticalRatePerBullet, int numberOfBullets, float bulletSpeedScale, float stagger)
+        {
+            const float delayEachBullet = 0.1f;
+            
+            for (var i = 0; i < numberOfBullets; i++)
+            {
+                var dir = target - spawnPos;
+                var p = ProjectilePool.Instance.Get(projectilePrefab, null, false);
+                p.transform.position = spawnPos;
+                p.Init(spawnPos, dir.normalized, bulletRange, bulletSpeedScale, damagePerBullet, criticalDamagePerBullet, criticalRatePerBullet, stagger);
+                p.Activate(delayEachBullet * i);
+                
+                var pDir = (Vector2)(Quaternion.Euler(0f, 0f, angle / 2) * dir);
+                p = ProjectilePool.Instance.Get(projectilePrefab, null, false);
+                p.transform.position = spawnPos;
+                p.Init(spawnPos, pDir.normalized, bulletRange, bulletSpeedScale, damagePerBullet, criticalDamagePerBullet, criticalRatePerBullet, stagger);
+                p.Activate(delayEachBullet * i);
+            
+                pDir = Quaternion.Euler(0f, 0f, - angle / 2) * dir;
+                p = ProjectilePool.Instance.Get(projectilePrefab, null, false);
+                p.transform.position = spawnPos;
+                p.Init(spawnPos, pDir.normalized, bulletRange, bulletSpeedScale, damagePerBullet, criticalDamagePerBullet, criticalRatePerBullet, stagger);
+                p.Activate(delayEachBullet * i);
+            }
+            
+        }
+    }
+}
