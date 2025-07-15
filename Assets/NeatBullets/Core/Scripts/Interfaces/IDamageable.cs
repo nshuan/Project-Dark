47ca9427869f9bd144dc3392c8f05502d5d6665b
norman <nshuan8@gmail.com>
2025-07-15 23:45:43 +0700

    remove unused plugin

--- Assets/NeatBullets/Core/Scripts/Interfaces/IDamageable.cs
+++
@@ -1,9 +0,0 @@
-namespace NeatBullets.Core.Scripts.Interfaces {
-    
-    public interface IDamagable {
-        public float HealthPoints { get; set; }
-        public void TakeDamage(float damage);
-    }
-}
-
-
