d385351f59525de2bb63112d26d703c58401ede9
norman <nshuan8@gmail.com>
2025-08-05 20:16:26 +0700

    add ui wave process

--- Assets/Dark/Scripts/InGame/UI/Waves/UIWaveProcess.cs
+++ Assets/Dark/Scripts/InGame/UI/Waves/UIWaveProcess.cs
@@ -1,4 +1,5 @@
 using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace InGame.UI.Waves
@@ -6,8 +7,10 @@ namespace InGame.UI.Waves
     public class UIWaveProcess : MonoBehaviour
     {
         [SerializeField] private UIWaveProcessItem[] waveItems;
+        [SerializeField] private Vector2 anchor;
+        [SerializeField] private float spacing;
 
-        private int totalWave;
+        private int totalWave = 10;
         
         private void Awake()
         {
@@ -31,6 +34,7 @@ namespace InGame.UI.Waves
 
         // 0 - 1 - 2 - 3 - 4
         // Current state = 3
+        [Button]
         public void UpdateUI(int currentWave)
         {
             for (var i = 0; i < waveItems.Length; i++)
@@ -43,6 +47,30 @@ namespace InGame.UI.Waves
                 else
                 {
                     waveItems[i].gameObject.SetActive(false);
+                }
+            }
+
+            var currentPos = anchor;
+            for (var i = 0; i < Math.Min(waveItems.Length, totalWave); i++)
+            {
+                waveItems[i].transform.localPosition = currentPos;
+                if (i < waveItems.Length - 1)
+                {
+                    currentPos.y -= (waveItems[i].rectTransform.sizeDelta.y / 2 + spacing + waveItems[i + 1].rectTransform.sizeDelta.y / 2);
+                }
+            }
+        }
+
+        [Button]
+        public void Validate()
+        {
+            var currentPos = anchor;
+            for (var i = 0; i < waveItems.Length; i++)
+            {
+                waveItems[i].transform.localPosition = currentPos;
+                if (i < waveItems.Length - 1)
+                {
+                    currentPos.y -= (waveItems[i].rectTransform.sizeDelta.y / 2 + spacing + waveItems[i + 1].rectTransform.sizeDelta.y / 2);
                 }
             }
         }
--- Assets/Dark/Scripts/InGame/UI/Waves/UIWaveProcessItem.cs
+++ Assets/Dark/Scripts/InGame/UI/Waves/UIWaveProcessItem.cs
@@ -5,14 +5,24 @@ namespace InGame.UI.Waves
 {
     public class UIWaveProcessItem : MonoBehaviour
     {
-        [SerializeField] private GameObject[] states;
-        
+        [SerializeField] private RectTransform[] states;
+        public RectTransform rectTransform;
+
+        private void Awake()
+        {
+            rectTransform = (RectTransform)transform;
+        }
+
         public void UpdateUI(int stateIndex)
         {
             stateIndex = Math.Clamp(stateIndex, 0, states.Length - 1);
             for (var i = 0; i < states.Length; i++)
             {
-                states[i].SetActive(i == stateIndex);
+                states[i].gameObject.SetActive(i == stateIndex);
+                if (i == stateIndex)
+                {
+                    rectTransform.sizeDelta = states[i].sizeDelta;
+                }
             }
         }
     }
