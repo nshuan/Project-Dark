bab0ccd064cb4dbb726d6e66623c1b44034ca952
NaN <nshuan8@gmail.com>
2025-09-16 18:41:51 +0700

    update tower regen on kill

--- Assets/Dark/Scripts/InGame/Tower/TowerRegenerateOnKill.cs
+++ Assets/Dark/Scripts/InGame/Tower/TowerRegenerateOnKill.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Dark.Scripts.Common.ParticleSystemUtil;
 using UnityEngine;
 
 namespace InGame
@@ -6,6 +8,12 @@ namespace InGame
     {
         private TowerEntity tower;
         private int regenAmount;
+        
+        private Coroutine coroutineRegenerate;
+        
+        [Header("Visual")]
+        [SerializeField] private ParticleSystemGroup vfxTowerRegenerate;
+        [SerializeField] private float vfxDuration;
         
         public void Initialize(TowerEntity targetTower, int amount)
         {
@@ -17,7 +25,19 @@ namespace InGame
 
         private void OnOneEnemyKilled()
         {
+            if (coroutineRegenerate != null)
+                StopCoroutine(coroutineRegenerate);
+            coroutineRegenerate = StartCoroutine(IERegenerate());
+        }
+        
+        private IEnumerator IERegenerate()
+        {
+            vfxTowerRegenerate.gameObject.SetActive(true);
+            
             tower.Regenerate(regenAmount);
+            
+            yield return new WaitForSeconds(vfxDuration);
+            vfxTowerRegenerate.gameObject.SetActive(false);
         }
     }
 }
