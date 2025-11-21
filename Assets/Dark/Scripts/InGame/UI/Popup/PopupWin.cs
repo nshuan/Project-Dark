9ebafd32661b7939b924b7b0ed2700c04157d1fa
NaN <nshuan8@gmail.com>
2025-11-21 15:35:16 +0700

    update popup win, popup lose

--- Assets/Dark/Scripts/InGame/UI/Popup/PopupWin.cs
+++ Assets/Dark/Scripts/InGame/UI/Popup/PopupWin.cs
@@ -4,6 +4,8 @@ using Dark.Scripts.SceneNavigation;
 using Data;
 using DG.Tweening;
 using Economic;
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -29,14 +31,21 @@ namespace InGame.UI
             onShowPopup = null;
         }
 
+        [Button]
         private void OnWin()
         {
             UpdateUI();
-            ui.DoOpenFadeIn(0f).OnComplete(() => onShowPopup?.Invoke());
+            ui.DoOpenFadeIn(0f).OnComplete(() =>
+            {
+                onShowPopup?.Invoke();
+                DoShowUIPopup();
+            });
         }
 
         private void UpdateUI()
         {
+            ResetPopupUI();
+            
             btnBackToTree.onClick.RemoveAllListeners();
             btnBackToTree.onClick.AddListener(() =>
             {
@@ -53,6 +62,86 @@ namespace InGame.UI
                     LevelManager.Instance.LoadLevel(PlayerDataManager.Instance.Data.level + 1);
                 });
             });
+        }
+        
+        [Space] [Header("UI Tween")] 
+        
+        [SerializeField] private Image imgTitle;
+        [SerializeField] private Image imgTitleBg;
+        [SerializeField] private TextMeshProUGUI txtDescription;
+        [SerializeField] private TextMeshProUGUI txtTitleResourceCollected;
+        [SerializeField] private CanvasGroup groupResourceCollected;
+        [SerializeField] private CanvasGroup groupTimePlayed;
+        [SerializeField] private Transform rectLine;
+        [SerializeField] private CanvasGroup groupBtnBackToTree;
+        [SerializeField] private CanvasGroup groupBtnReplay;
+
+        private void ResetPopupUI()
+        {
+            imgTitle.SetAlpha(0f);
+            imgTitleBg.SetAlpha(0f);
+            txtDescription.SetAlpha(0f);
+            txtTitleResourceCollected.SetAlpha(0f);
+            groupResourceCollected.alpha = 0f;
+            groupTimePlayed.alpha = 0f;
+            rectLine.localScale = new Vector3(0f, 1f, 1f);
+            groupBtnBackToTree.alpha = 0f;
+            groupBtnReplay.alpha = 0f;
+        }
+        
+        private Tween DoShowUIPopup()
+        {
+            DOTween.Kill(ui);
+            var seq = DOTween.Sequence(ui).SetUpdate(true);
+
+            seq.AppendCallback(ResetPopupUI)
+                .AppendCallback((() =>
+                {
+                    imgTitleBg.DOFade(1f, 0.3f).SetUpdate(true);
+                    imgTitle.DOFade(1f, 0.7f).SetUpdate(true).SetEase(Ease.OutQuad).SetDelay(0.1f);
+                }))
+                .AppendInterval(0.3f)
+                .AppendCallback(() =>
+                {
+                    txtDescription.transform.localPosition += new Vector3(0f, 10f, 0f);
+                    txtDescription.transform.DOLocalMoveY(-10f, 0.5f).SetUpdate(true).SetRelative(true);
+                    txtDescription.DOFade(1f, 0.5f).SetUpdate(true);
+                })
+                .AppendInterval(0.8f)
+                .AppendCallback(() =>
+                {
+                    txtTitleResourceCollected.transform.localPosition += new Vector3(0f, 10f, 0f);
+                    txtTitleResourceCollected.transform.DOLocalMoveY(-10f, 0.5f).SetUpdate(true).SetRelative(true);
+                    txtTitleResourceCollected.DOFade(1f, 0.5f).SetUpdate(true);
+                    
+                    groupResourceCollected.transform.localPosition += new Vector3(0f, 10f, 0f);
+                    groupResourceCollected.transform.DOLocalMoveY(-10f, 0.5f).SetUpdate(true).SetRelative(true).SetDelay(0.2f);
+                    groupResourceCollected.DOFade(1f, 0.5f).SetUpdate(true).SetDelay(0.2f);
+                    
+                    groupTimePlayed.transform.localPosition += new Vector3(0f, 10f, 0f);
+                    groupTimePlayed.transform.DOLocalMoveY(-10f, 0.5f).SetUpdate(true).SetRelative(true).SetDelay(0.4f);
+                    groupTimePlayed.DOFade(1f, 0.5f).SetUpdate(true).SetDelay(0.4f);
+                })
+                .AppendInterval(1f)
+                .Append(rectLine.DOScaleX(1f, 0.3f))
+                .AppendCallback(() =>
+                {
+                    groupBtnBackToTree.transform.localPosition += new Vector3(0f, 10f, 0f);
+                    groupBtnBackToTree.transform.DOLocalMoveY(-10f, 0.5f).SetUpdate(true).SetRelative(true);
+                    groupBtnBackToTree.DOFade(1f, 0.5f).SetUpdate(true);
+                    
+                    groupBtnReplay.transform.localPosition += new Vector3(0f, 10f, 0f);
+                    groupBtnReplay.transform.DOLocalMoveY(-10f, 0.5f).SetUpdate(true).SetRelative(true);
+                    groupBtnReplay.DOFade(1f, 0.5f).SetUpdate(true);
+                });
+            
+            return seq;
+        }
+
+        [Button]
+        private void TestPlay()
+        {
+            DoShowUIPopup();
         }
     }
 }
