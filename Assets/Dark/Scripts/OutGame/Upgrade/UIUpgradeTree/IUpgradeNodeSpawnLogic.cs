9197840d4cd47b2de24cb81924ea3ce52aa13cb5
norman <nshuan8@gmail.com>
2025-11-29 01:17:00 +0700

    fix prefab node class

--- Assets/Dark/Scripts/OutGame/Upgrade/UIUpgradeTree/IUpgradeNodeSpawnLogic.cs
+++ Assets/Dark/Scripts/OutGame/Upgrade/UIUpgradeTree/IUpgradeNodeSpawnLogic.cs
@@ -1,4 +1,5 @@
 using System;
+using Coffee.UIExtensions;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
@@ -38,6 +39,50 @@ namespace Dark.Scripts.OutGame.Upgrade
                 })
                 .Append(imgFlash.DOFade(0f, 0.3f).SetEase(Ease.OutQuad))
                 .AppendCallback(() => imgFlash.gameObject.SetActive(false));
+        }
+    }
+    
+    [Serializable]
+    public class NodeSpawnRotateAndGlow : IUpgradeNodeSpawnLogic
+    {
+        [SerializeField] private Image imgBorder;
+        [SerializeField] private UIParticle vfxSpawn;
+        
+        public Tween DoSpawn()
+        {
+            imgBorder.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            imgBorder.transform.localScale = Vector3.one;
+            
+            return DOTween.Sequence(this)
+                .AppendCallback(() =>
+                {
+                    DOVirtual.DelayedCall(0.2f, () => vfxSpawn.gameObject.SetActive(true));
+                })
+                .Append(imgBorder.transform.DOLocalRotate(new Vector3(0f, 0f, 180f), 0.4f))
+                .Join(imgBorder.transform.DOScale(1.1f, 0.4f).SetEase(Ease.OutQuad))
+                .Append(imgBorder.transform.DOScale(1f, 0.2f).SetEase(Ease.InQuad));
+        }
+    }
+    
+    [Serializable]
+    public class NodeSpawnGlowVfx : IUpgradeNodeSpawnLogic
+    {
+        [SerializeField] private CanvasGroup groupNode;
+        [SerializeField] private UIParticle vfxSpawn;
+        [SerializeField] private bool autoPlay = false;
+        
+        public Tween DoSpawn()
+        {
+            groupNode.alpha = 0f;
+            
+            return DOTween.Sequence()
+                .AppendCallback(() =>
+                {
+                    vfxSpawn.gameObject.SetActive(true);
+                    groupNode.alpha = 1f;
+                    if (!autoPlay) 
+                        vfxSpawn.Play();
+                });
         }
     }
 }
