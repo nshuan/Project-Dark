592d8f14359c276ae450de5b90b9b5c8e1e0014d
NaN <nshuan8@gmail.com>
2025-11-28 11:20:48 +0700

    update skill tree node hover

--- Assets/Dark/Scripts/OutGame/Upgrade/UIUpgradeTree/UIUpgradeNodeHoverField.cs
+++ Assets/Dark/Scripts/OutGame/Upgrade/UIUpgradeTree/UIUpgradeNodeHoverField.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
@@ -7,31 +8,39 @@ namespace Dark.Scripts.OutGame.Upgrade
 {
     public class UIUpgradeNodeHoverField : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
     {
+        [SerializeField] private GameObject imgIconHovering;
+        
         public Action onHover;
         public Action onHoverExit;
         public Action onPointerClick;
         
         public RectTransform nodeRepresentableRect;
         public bool interactable;
-
+        public bool canShowIconHovering;
+        
         private void Awake()
         {
             nodeRepresentableRect ??= GetComponent<RectTransform>();
+            canShowIconHovering = imgIconHovering != null;
+            if (canShowIconHovering) imgIconHovering.SetActive(false);
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
+            if (canShowIconHovering) imgIconHovering.SetActive(true);
             onHover?.Invoke();
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
+            if (canShowIconHovering) imgIconHovering.SetActive(false);
             onHoverExit?.Invoke();
         }
 
         public void OnPointerClick(PointerEventData eventData)
         {
             if (!interactable) return;
+            if (canShowIconHovering) imgIconHovering.transform.DOPunchScale(new Vector3(0.1f, 0.1f, 0.1f), 0.3f);
             onPointerClick?.Invoke();
         }
     }
