7daa80d4a12a16b17c4f08f6f4c8080f1df02269
norman <nshuan8@gmail.com>
2025-11-30 21:52:01 +0700

    show full tree

--- Assets/Dark/Scripts/InGame/GameConst.cs
+++ Assets/Dark/Scripts/InGame/GameConst.cs
@@ -6,7 +6,7 @@ namespace InGame
         public const float EnemyEliteScale = 1.5f; // scale normal enemy to be elite    
 
         public const bool DefaultAutoAttack = true; // Default enable auto-attack
-        public const bool HideLockedNode = true; // Hide locked nodes in skill tree
+        public const bool HideLockedNode = false; // Hide locked nodes in skill tree
         public static bool HideLaserWaveOnSpawnTree = true; // Hide laser wave on tree spawn animation
     }
 }
