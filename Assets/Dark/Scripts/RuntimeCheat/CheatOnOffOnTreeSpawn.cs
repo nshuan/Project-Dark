55e104ed674f535f7a6068d84b8c2d47045ae6a4
norman <nshuan8@gmail.com>
2025-12-03 22:44:29 +0700

    update skill tree, skill preview

--- Assets/Dark/Scripts/RuntimeCheat/CheatOnOffOnTreeSpawn.cs
+++ Assets/Dark/Scripts/RuntimeCheat/CheatOnOffOnTreeSpawn.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 namespace Dark.Scripts.RuntimeCheat
 {
-    public class CheatOnOffLaserWaveOnTreeSpawn : MonoBehaviour
+    public class CheatOnOffOnTreeSpawn : MonoBehaviour
     {
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.O))
             {
                 GameConst.HideLaserWaveOnSpawnTree = !GameConst.HideLaserWaveOnSpawnTree;
+            }
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                GameConst.HideLockedNode = !GameConst.HideLockedNode;
             }
         }
     }
