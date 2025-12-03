94e73372b51e8cbd9bc861e54725cf75c2a4fe2c
NaN <nshuan8@gmail.com>
2025-12-03 11:31:54 +0700

    add base logic for supabase log

--- Assets/Dark/Scripts/InGame/GameConst.cs
+++ Assets/Dark/Scripts/InGame/GameConst.cs
@@ -2,6 +2,7 @@ public class GameConst
 {
     public const string SteamWishlistURL = "https://store.steampowered.com/app/3913310/Ash_Warden/";
     public const int MaxDemoLevel = 3; // Tester can only play 3 levels in demo version
+    public const bool EnableLogManagerDebugLog = true; // Enable debug log for LogManager
     
     public const float IsoRatio = 0.5837f; // max height / max width 
     public const float EnemyEliteScale = 1.5f; // scale normal enemy to be elite    
