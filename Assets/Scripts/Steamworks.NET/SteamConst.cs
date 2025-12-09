11ff6d22947e3c8a64bf6e33e011477ddef4aa71
norman <nshuan8@gmail.com>
2025-12-10 00:37:31 +0700

    update appId

--- Assets/Scripts/Steamworks.NET/SteamConst.cs
+++ Assets/Scripts/Steamworks.NET/SteamConst.cs
@@ -1,4 +1,5 @@
 public class SteamConst
 {
-    public const int AppId = 480;
+    // public const int AppId = 480; // AppId for Space War
+    public const int AppId = 4139710; // AppId for play test
 }
