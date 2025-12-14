f72a1aac95641e399387ee82c03c1e372618dd4e
norman <nshuan8@gmail.com>
2025-12-14 16:56:27 +0700

    update tag, tutorial collect

--- Assets/Dark/Scripts/Tutorial/StepsInGame/UITutorialStepCollect.cs
+++ Assets/Dark/Scripts/Tutorial/StepsInGame/UITutorialStepCollect.cs
@@ -61,11 +61,11 @@ namespace Dark.Scripts.Tutorial
             if (!instructionTarget) return;
             if (!mainCamera) return;
             objInstruction.transform.position = mainCamera.WorldToScreenPoint(instructionTarget.position);
-            // actionUpdateFocus?.Invoke(
-            //     objInstruction.transform.localPosition - new Vector3(0f, 96f, 0f),
-            //     new Vector2(0.03f, 0.05f),
-            //     2f,
-            //     false, false);
+            actionUpdateFocus?.Invoke(
+                objInstruction.transform.localPosition,
+                new Vector2(0.03f, 0.05f),
+                1.5f,
+                true, true);
         }
     }
 }
