725a5426a9fb4ebbbe62efcfceb06cf23ee75063
norman <nshuan8@gmail.com>
2025-12-21 17:49:39 +0700

    update boss config

--- Assets/Dark/Tools/GoogleSheetTools/GoogleSheetTabs.cs
+++ Assets/Dark/Tools/GoogleSheetTools/GoogleSheetTabs.cs
@@ -8,11 +8,12 @@ namespace Dark.Tools.GoogleSheetTool
         SkillConfig = 69138666, // "SkillConfig"
         PassiveConfig = 545721731, // "PassiveConfig"
         MoveConfig = 545988098, // "MoveConfig"
+        TowerCounter = 1956116879, // "TowerCounter"
         NodeConfig = 1947278781, // "NodeConfig"
         CostConfig = 677348826, // "CostConfig"
         LevelUpConfig = 468570559, // "LevelUpConfig"
         Wave = 230897661, // "Wave"
         LevelConfig = 992670229, // "LevelConfig"
-        TowerCounter = 1956116879, // "TowerCounter"
+        BossConfig = 181237602, // "BossConfig"
     }
 }
