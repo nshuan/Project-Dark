9baea233b17386f8b9109abcc88eae9c097b5033
NaN <nshuan8@gmail.com>
2025-12-22 13:00:51 +0700

    add config for spawn angle span

--- Assets/Dark/Scripts/InGame/Gate/IGateSpawner.cs
+++ Assets/Dark/Scripts/InGame/Gate/IGateSpawner.cs
@@ -14,7 +14,7 @@ namespace InGame
     public class GateSpawnSingle : IGateSpawner
     {
         [Range(1f, 2f)] public float radius = 1.5f;
-        [Range(10f, 90f)] public float randomSpan = 90f;
+        public float randomSpanAngle = 90f;
         
         public (EnemyEntity, TowerEntity)[] Spawn(Vector2 gatePosition, int enemyId, EnemyEntity enemyPrefab, TowerEntity[] targetTower)
         {
@@ -24,7 +24,7 @@ namespace InGame
             target = targetTower[RandomUtil.Range(0, targetTower.Length)];
             enemy.transform.position =
                 (Vector3)gatePosition + 
-                (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpan / 2, randomSpan / 2)) * (target.transform.position - (Vector3)gatePosition).normalized)
+                (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpanAngle / 2, randomSpanAngle / 2)) * (target.transform.position - (Vector3)gatePosition).normalized)
                 * radius;
             
             return new [] { (enemy, target) };
@@ -35,7 +35,7 @@ namespace InGame
     public class GateSpawnTriangle : IGateSpawner
     {
         [Range(1f, 2f)] public float radius = 1.5f;
-        [Range(10f, 90f)] public float randomSpan = 90f;
+        public float randomSpanAngle = 90f;
         
         public (EnemyEntity, TowerEntity)[] Spawn(Vector2 gatePosition, int enemyId, EnemyEntity enemyPrefab, TowerEntity[] targetTower)
         {
@@ -46,21 +46,21 @@ namespace InGame
             targets[0] = targetTower[RandomUtil.Range(0, targetTower.Length)];
             enemies[0].transform.position =
                 (Vector3)gatePosition + 
-                (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpan / 2, randomSpan / 2)) * (targets[0].transform.position - (Vector3)gatePosition).normalized)
+                (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpanAngle / 2, randomSpanAngle / 2)) * (targets[0].transform.position - (Vector3)gatePosition).normalized)
                 * radius;
 
             enemies[1] = EnemyPool.Instance.Get(enemyPrefab, enemyId, null, false);
             targets[1] = targetTower[RandomUtil.Range(0, targetTower.Length)];
             enemies[1].transform.position =
                 (Vector3)gatePosition + 
-                (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpan / 2, randomSpan / 2)) * (targets[1].transform.position - (Vector3)gatePosition).normalized)
+                (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpanAngle / 2, randomSpanAngle / 2)) * (targets[1].transform.position - (Vector3)gatePosition).normalized)
                 * radius;
             
             enemies[2] = EnemyPool.Instance.Get(enemyPrefab, enemyId, null, false);
             targets[2] = targetTower[RandomUtil.Range(0, targetTower.Length)];
             enemies[2].transform.position =
                 (Vector3)gatePosition + 
-                (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpan / 2, randomSpan / 2)) * (targets[2].transform.position - (Vector3)gatePosition).normalized)
+                (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpanAngle / 2, randomSpanAngle / 2)) * (targets[2].transform.position - (Vector3)gatePosition).normalized)
                 * radius;
             
             return new []
@@ -77,7 +77,7 @@ namespace InGame
     {
         [Range(1, 10)] public int amount = 1;
         [Range(1f, 2f)] public float maxRadius = 1.8f;
-        [Range(10f, 120f)] public float randomSpan = 120f;
+        public float randomSpanAngle = 120f;
         
         public (EnemyEntity, TowerEntity)[] Spawn(Vector2 gatePosition, int enemyId, EnemyEntity enemyPrefab, TowerEntity[] targetTower)
         {
@@ -91,7 +91,7 @@ namespace InGame
                 targets[i] = targetTower[RandomUtil.Range(0, targetTower.Length)];
                 enemies[i].transform.position =
                     (Vector3)gatePosition +
-                    (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpan / 2, randomSpan / 2)) *
+                    (Quaternion.Euler(0f, 0f, RandomUtil.Range(-randomSpanAngle / 2, randomSpanAngle / 2)) *
                      (targets[i].transform.position - (Vector3)gatePosition).normalized)
                     * RandomUtil.Range(maxRadius - 0.3f, maxRadius);
                 result[i] = (enemies[i], targets[i]);
