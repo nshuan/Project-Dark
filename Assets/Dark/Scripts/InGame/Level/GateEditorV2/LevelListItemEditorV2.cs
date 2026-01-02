485c4cd42e3caf55a81a77185d38bc379066782f
norman <nshuan8@gmail.com>
2026-01-02 18:54:39 +0700

    update level design tool
    add delete buttons

--- Assets/Dark/Scripts/InGame/Level/GateEditorV2/LevelListItemEditorV2.cs
+++ Assets/Dark/Scripts/InGame/Level/GateEditorV2/LevelListItemEditorV2.cs
@@ -9,8 +9,12 @@ namespace InGame.GateEditorV2
         public TextMeshProUGUI txtLevel;
         public Button btnClick;
         
+        public LevelConfig Config { get; set; }
+        public bool Selecting { get; set; }
+        
         public void UpdateUI(LevelConfig level)
         {
+            Config = level;
             txtLevel.SetText($"Level {level.level}");
             btnClick.onClick.RemoveAllListeners();
             btnClick.onClick.AddListener(() =>
