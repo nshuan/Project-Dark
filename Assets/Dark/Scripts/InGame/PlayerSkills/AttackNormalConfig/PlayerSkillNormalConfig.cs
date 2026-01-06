8d162c76529f8659e0853511337c9fdf76e4b893
norman <nshuan8@gmail.com>
2026-01-06 17:32:25 +0700

    add config for split bullet angle

--- Assets/Dark/Scripts/InGame/PlayerSkills/AttackNormalConfig/PlayerSkillNormalConfig.cs
+++ Assets/Dark/Scripts/InGame/PlayerSkills/AttackNormalConfig/PlayerSkillNormalConfig.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using UnityEngine;
 
 namespace InGame.AttackNormalConfig
@@ -8,6 +10,22 @@ namespace InGame.AttackNormalConfig
         public int id;
         public float dmgScale;
         public float amount;
+        public NormalBulletAngleSpanInfo[] angleSpanInfos; // For normal attack bullet
+
+        public int GetNormalBulletSpanAngle(int numberOfBullets)
+        {
+            if (angleSpanInfos == null || angleSpanInfos.Length == 0) return 30;
+            var info = angleSpanInfos.FirstOrDefault((span) => span.numberOfBullets == numberOfBullets);
+            if (info == null) return 30;
+            return info.angleSpan;
+        }
+        
+        [Serializable]
+        public class NormalBulletAngleSpanInfo
+        {
+            public int numberOfBullets;
+            public int angleSpan;
+        }
     }
     
     public enum NormalType
