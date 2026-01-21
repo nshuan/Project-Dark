5bfbdedd8d342a3d1845c43fd1ea8f410e0102b4
norman <nshuan8@gmail.com>
2026-01-22 00:39:23 +0700

    update steam manager

--- Assets/Scripts/Steamworks.NET/SteamConst.cs
+++ Assets/Scripts/Steamworks.NET/SteamConst.cs
@@ -2,4 +2,5 @@ public class SteamConst
 {
     // public const int AppId = 480; // AppId for Space War
     public const int AppId = 4139710; // AppId for play test
+    // public const int AppId = 4139700; // AppId for demo
 }
