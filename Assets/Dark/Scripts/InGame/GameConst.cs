b72be7e94bbdabe5817e15cb009222c0611ef43f
norman <nshuan8@gmail.com>
2026-01-25 14:24:42 +0700

    update ui ingame

--- Assets/Dark/Scripts/InGame/GameConst.cs
+++ Assets/Dark/Scripts/InGame/GameConst.cs
@@ -14,6 +14,7 @@ public class GameConst
     public const float IsoRatio = 0.5837f; // max height / max width 
     public const float EnemyEliteScale = 1.5f; // scale normal enemy to be elite    
     public const bool DefaultAutoAttack = true; // Default enable auto-attack
+    public const bool DefaultShowPassiveIcon = false;
     
     public static bool HideLockedNode = false; // Hide locked nodes in skill tree
     public static bool HideLaserWaveOnSpawnTree = true; // Hide laser wave on tree spawn animation
