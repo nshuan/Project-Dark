4bb6afb97579e119c0023e53bcb2e7660a732390
norman <nshuan8@gmail.com>
2026-01-28 01:01:54 +0700

    update range enemy

--- Assets/Dark/Scripts/InGame/EnemyEntity.cs
+++ Assets/Dark/Scripts/InGame/EnemyEntity.cs
@@ -184,10 +184,8 @@ namespace InGame
                 healthBar.gameObject.SetActive(!IsBoss ? GameSettings.ShowEnemyHealth : GameSettings.ShowBossHealth);
                 if (showAttackRange)
                 {
-                    DOTween.Kill(visualAttackRange);
-                    visualAttackRange.localScale = Vector3.zero;
+                    visualAttackRange.localScale = AttackRange * Vector3.one;
                     visualAttackRange.gameObject.SetActive(true);
-                    visualAttackRange.DOScale(AttackRange, 0.5f).SetEase(Ease.OutQuad).SetTarget(visualAttackRange);
                 }
             });
         }
@@ -400,15 +398,14 @@ namespace InGame
             yield return new WaitForEndOfFrame();
             // Đợi chạy xong anim hit rồi mới chạy anim die
             shadow.SetActive(false);    
-            if (showAttackRange)
-            {
-                DOTween.Kill(visualAttackRange);
-                visualAttackRange.DOScale(0f, 0.5f).SetEase(Ease.InQuad).SetTarget(visualAttackRange);
-            }
             OnStartDead?.Invoke();
             OnStartDead = null;
             yield return new WaitForSeconds(delayDieAnimation);
             yield return new WaitForSeconds(animController.PlayDie());
+            if (showAttackRange)
+            {
+                visualAttackRange.gameObject.SetActive(false);
+            }
             OnDead?.Invoke(reason);
             OnDead = null;
             yield return new WaitForSeconds(delayRelease);
