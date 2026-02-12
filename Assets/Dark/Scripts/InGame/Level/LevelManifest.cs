0aa85b5ea267dd66a1740e6726ac73f042bab524
norman <nshuan8@gmail.com>
2026-02-13 00:48:11 +0700

    update level

--- Assets/Dark/Scripts/InGame/Level/LevelManifest.cs
+++ Assets/Dark/Scripts/InGame/Level/LevelManifest.cs
@@ -17,29 +17,61 @@ namespace InGame
         private static string FilePath = "Assets/Dark/Resources/LevelManifest.asset";
         
         [ReadOnly, NonSerialized, OdinSerialize]
-        private Dictionary<int, LevelConfig> levelMap;
+        private Dictionary<int, LevelConfig> archerLevelMap;
+        
+        [ReadOnly, NonSerialized, OdinSerialize]
+        private int maxLevelArcher;
 
         [ReadOnly, NonSerialized, OdinSerialize]
-        private int maxLevel;
+        private Dictionary<int, LevelConfig> knightLevelMap;
 
-        public LevelConfig GetLevel(int level)
+        [ReadOnly, NonSerialized, OdinSerialize]
+        private int maxLevelKnight;
+
+        public LevelConfig GetLevel(CharacterClass.CharacterClass classType, int level)
         {
-            if (levelMap.TryGetValue(level, out var value)) return Instantiate(value);
+            if (classType == CharacterClass.CharacterClass.Archer)
+            {
+                if (archerLevelMap.TryGetValue(level, out var value)) return Instantiate(value);
+            }
+            else if (classType == CharacterClass.CharacterClass.Knight)
+            {
+                if (knightLevelMap.TryGetValue(level, out var value)) return Instantiate(value);
+            }
             return null;
         }
 
-        public LevelConfig GetTrueLevel(int level)
+        public LevelConfig GetTrueLevel(CharacterClass.CharacterClass classType, int level)
         {
-            if (levelMap.TryGetValue(level, out var value)) return value;
+            if (classType == CharacterClass.CharacterClass.Archer)
+            {
+                if (archerLevelMap.TryGetValue(level, out var value)) return value;
+            }
+            else if (classType == CharacterClass.CharacterClass.Knight)
+            {
+                if (knightLevelMap.TryGetValue(level, out var value)) return value;
+            }
             return null;
         }
 
-        public LevelConfig[] GetAllLevels()
+        public LevelConfig[] GetAllLevels(CharacterClass.CharacterClass classType)
         {
-            if (levelMap == null)
-                return null;
+            if (classType == CharacterClass.CharacterClass.Archer)
+            {
+                if (archerLevelMap == null)
+                    return null;
             
-            return levelMap.Values.ToArray();
+                return archerLevelMap.Values.ToArray();
+            }
+            else if (classType == CharacterClass.CharacterClass.Knight)
+            {
+                if (knightLevelMap == null)
+                    return null;
+                
+                return knightLevelMap.Values.ToArray();
+            }
+
+            return null;
         }
         
         #region SINGLETON
@@ -59,8 +91,10 @@ namespace InGame
         #endregion
         
 #if UNITY_EDITOR
-        public const string LevelPath = "Assets/Dark/Config/LevelInGame";
-        public const string WavePath = "Assets/Dark/Config/LevelWaveInGame";
+        public const string ArcherLevelPath = "Assets/Dark/Config/LevelArcherInGame";
+        public const string ArcherWavePath = "Assets/Dark/Config/LevelWaveArcherInGame";
+        public const string KnightLevelPath = "Assets/Dark/Config/LevelKnightInGame";
+        public const string KnightWavePath = "Assets/Dark/Config/LevelWaveKnightInGame";
 
         [MenuItem("Dark/Manifest/Generate Level Manifest")]
         public static void CreateInstance()
@@ -73,9 +107,9 @@ namespace InGame
         {
             Debug.ClearDeveloperConsole();
             
-            var levels = AssetUtility.LoadAllScriptableObjectsInFolder<LevelConfig>(LevelPath).ToList();
-            levelMap = new Dictionary<int, LevelConfig>();
-            foreach (var level in levels)
+            var archerLevels = AssetUtility.LoadAllScriptableObjectsInFolder<LevelConfig>(ArcherLevelPath).ToList();
+            archerLevelMap = new Dictionary<int, LevelConfig>();
+            foreach (var level in archerLevels)
             {
                 if (int.TryParse(level.name.Split(" ")[1], out var levelNum))
                 {
@@ -90,21 +124,57 @@ namespace InGame
                     DebugUtility.LogError($"Level {level.name} has invalid level name!");
                 }
                     
-                if (levelMap.ContainsKey(level.level))
+                if (archerLevelMap.ContainsKey(level.level))
                 {
                     DebugUtility.LogError($"Level {level.name} has invalid level index!");
                     continue;
                 }
                 
-                levelMap.Add(level.level, level);
+                archerLevelMap.Add(level.level, level);
             }
 
-            levelMap = levelMap.OrderBy((pair) => pair.Key).ToDictionary((pair) => pair.Key, pair => pair.Value);
-            maxLevel = levelMap.Keys.Max();
+            archerLevelMap = archerLevelMap.OrderBy((pair) => pair.Key).ToDictionary((pair) => pair.Key, pair => pair.Value);
+            maxLevelArcher = archerLevelMap.Keys.Max();
 
-            for (var i = 1; i <= maxLevel; i++)
+            for (var i = 1; i <= maxLevelArcher; i++)
             {
-                if (!levelMap.ContainsKey(i))
+                if (!archerLevelMap.ContainsKey(i))
+                    DebugUtility.LogError($"Level {i} is missing!");
+            }
+            
+            // Knight
+            var knightLevels = AssetUtility.LoadAllScriptableObjectsInFolder<LevelConfig>(KnightLevelPath).ToList();
+            knightLevelMap = new Dictionary<int, LevelConfig>();
+            foreach (var level in knightLevels)
+            {
+                if (int.TryParse(level.name.Split(" ")[1], out var levelNum))
+                {
+                    if (level.level != levelNum)
+                    {
+                        level.level = levelNum;
+                        EditorUtility.SetDirty(level);
+                    }
+                }
+                else
+                {
+                    DebugUtility.LogError($"Level {level.name} has invalid level name!");
+                }
+                    
+                if (knightLevelMap.ContainsKey(level.level))
+                {
+                    DebugUtility.LogError($"Level {level.name} has invalid level index!");
+                    continue;
+                }
+                
+                knightLevelMap.Add(level.level, level);
+            }
+
+            knightLevelMap = knightLevelMap.OrderBy((pair) => pair.Key).ToDictionary((pair) => pair.Key, pair => pair.Value);
+            maxLevelKnight = knightLevelMap.Keys.Max();
+
+            for (var i = 1; i <= maxLevelKnight; i++)
+            {
+                if (!knightLevelMap.ContainsKey(i))
                     DebugUtility.LogError($"Level {i} is missing!");
             }
             
