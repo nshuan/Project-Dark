cea38aa468e11c72650ddb7a31b78c3c0f482f77
norman <nshuan8@gmail.com>
2026-03-04 16:08:54 +0700

    update settings

--- Assets/Dark/Scripts/Settings/MoveTower/UISettingItemMoveTowerKey.cs
+++ Assets/Dark/Scripts/Settings/MoveTower/UISettingItemMoveTowerKey.cs
@@ -9,10 +9,22 @@ namespace Dark.Scripts.Settings.MoveTower
     {
         [SerializeField] private TextMeshProUGUI txtTitle;
 
+        private void OnDestroy()
+        {
+            LanguageManager.Instance.UnregisterForceUpdate(UpdateTitle);
+        }
+        
         protected override void Start()
         {
             base.Start();
 
+            UpdateTitle();
+            
+            LanguageManager.Instance.RegisterForceUpdate(UpdateTitle);
+        }
+
+        private void UpdateTitle()
+        {
             if (_settingItemLogic is SettingMoveTowerKeyListener moveTowerKeyLogic)
             {
                 txtTitle.SetTextLanguage("key_settings_move_tower", ("%{value}", (moveTowerKeyLogic.keyIndex + 1).ToString()));
