c559f2f4fa66e22b8f372fa79a13383d483e513e
norman <nshuan8@gmail.com>
2026-03-30 13:27:49 +0700

    update endless editor

---
+++ Assets/Dark/Scripts/InGame/Level/EndlessEditor/EndlessLevelEditor.cs
@@ -0,0 +1,300 @@
+// using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Linq;
+// using Core;
+// using Dark.Scripts.Common.UIWarning;
+// using Dark.Scripts.SceneNavigation;
+// using Dark.Scripts.Utils;
+// using InGame.EndlessLevel;
+// using InGame.GateEditorV2;
+// using TMPro;
+// using UnityEditor;
+// using UnityEngine;
+// using UnityEngine.UI;
+//
+// namespace InGame.EndlessEditor
+// {
+//     public class EndlessLevelEditor : MonoSingleton<EndlessLevelEditor>
+//     {
+// #if UNITY_EDITOR
+//         public Camera cam;
+//         public RectTransform parentWaveButtons;
+//         public Button prefabWaveButton;
+//         public RectTransform parentWaves;
+//         public EndlessWaveEditor prefabWave;
+//         public Button btnSave;
+//         public Button btnAddWave;
+//         public Button btnDeleteWave;
+//         public UIPopupWarning popupConfirm;
+//         public Button btnPlayLevel;
+//
+//         [Space] [Header("Display")] 
+//         public TextMeshProUGUI txtLevel;
+//         
+//         public CharacterClass.CharacterClass ClassType { get; set; }
+//         
+//         private LevelEndlessConfig currentLevel;
+//         
+//         protected override void Awake()
+//         {
+//             base.Awake();
+//             
+//             btnSave.onClick.RemoveAllListeners();
+//             btnSave.onClick.AddListener(SaveLevel);
+//             btnAddWave.onClick.RemoveAllListeners();
+//             btnAddWave.onClick.AddListener(AddNewWave);
+//             btnDeleteWave.onClick.RemoveAllListeners();
+//             btnDeleteWave.onClick.AddListener(DeleteSelectingWave);
+//             btnPlayLevel.onClick.RemoveAllListeners();
+//             btnPlayLevel.onClick.AddListener(PlaySelectingLevel);
+//         }
+//         
+//         public void LoadLevel(LevelEndlessConfig level)
+//         {
+//             currentLevel = level;
+//             if (!currentLevel) return;
+//             
+//             // Destroy all old wave buttons
+//             ClearAllWaves();
+//             
+//             if (currentLevel.waveInfo == null) return;
+//             foreach (var waveInfo in currentLevel.waveInfo)
+//             {
+//                 AddNewWave(waveInfo);
+//             }
+//             SelectWave(0);
+//             
+//             txtLevel?.SetText($"Level: {levelId}");
+//         }
+//
+//         public void PlaySelectingLevel()
+//         {
+//             if (!currentLevel) return;
+//             
+// #if UNITY_EDITOR
+//             LevelManager.isLoadFromInit = true;
+// #endif
+//             this.DelayCall(0.5f, () =>
+//             {
+//                 Loading.Instance.QuickLoadScene(SceneConstants.SceneInGame, () =>
+//                 {
+//                     LevelManager.Instance.LoadLevel(currentLevel.level);
+//                 });
+//             });
+//         }
+//         
+//         #region Waves
+//
+//         public void ClearAllWaves()
+//         {
+//             foreach (Transform child in parentWaveButtons)
+//             {
+//                 if (child.name != btnAddWave.transform.name)
+//                     Destroy(child.gameObject);
+//             }
+//
+//             foreach (Transform child in parentWaves)
+//             {
+//                 Destroy(child.gameObject);
+//             }
+//         }
+//
+//         public void AddNewWave()
+//         {
+//             if (!currentLevel) return;
+//             var waveList = currentLevel.waveInfo != null ? currentLevel.waveInfo.ToList() : new List<WaveInfo>();
+//             waveList.Sort((wave1, wave2) => wave1.waveIndex.CompareTo(wave2.waveIndex));
+//
+//             var newWave = 0;
+//             if (waveList.Count > 0) newWave = waveList[^1].waveIndex + 1;
+//             var newWaveInfo = new WaveInfo()
+//             {
+//                 waveIndex = newWave
+//             };
+//             WaveConfig newWaveAsset;
+// #if UNITY_EDITOR
+//             if (!Directory.Exists(Path.Combine(WaveFolderPath, $"Level {currentLevel.level}")))
+//             {
+//                 Directory.CreateDirectory(Path.Combine(WaveFolderPath, $"Level {currentLevel.level}"));
+//             }
+//             var filePath = Path.Combine(WaveFolderPath,
+//                 $"Level {currentLevel.level}/Level_{currentLevel.level}_Wave_{newWave + 1}.asset");
+//             if (File.Exists(filePath))
+//             {
+//                 newWaveAsset = AssetDatabase.LoadAssetAtPath<WaveConfig>(filePath);
+//             }
+//             else
+//             {
+//                 newWaveAsset = ScriptableObject.CreateInstance<WaveConfig>();
+//                 AssetDatabase.CreateAsset(newWaveAsset, filePath);
+//                 AssetDatabase.SaveAssets();
+//                 AssetDatabase.Refresh();
+//             }
+//             
+//             newWaveInfo.waveConfig = newWaveAsset;
+//             waveList.Add(newWaveInfo);
+//             currentLevel.waveInfo = waveList.ToArray();
+//             EditorUtility.SetDirty(currentLevel);
+//             AssetDatabase.SaveAssets();
+//             AssetDatabase.Refresh();
+// #endif
+//             AddNewWave(newWaveInfo);
+//         }
+//
+//         public string WaveFolderPath
+//         {
+//             get
+//             {
+//                 if (ClassType == CharacterClass.CharacterClass.Knight) return LevelManifest.KnightWavePath;
+//                 return LevelManifest.ArcherWavePath;
+//             }
+//         }
+//         
+//         public string LevelFolderPath
+//         {
+//             get
+//             {
+//                 if (ClassType == CharacterClass.CharacterClass.Knight) return LevelManifest.KnightLevelPath;
+//                 return LevelManifest.ArcherLevelPath;
+//             }
+//         }
+//         
+//         public void AddNewWave(WaveInfo waveInfo)
+//         {
+//             var wave = Instantiate(prefabWave, parentWaves);
+//             wave.UpdateUI(waveInfo.waveConfig);
+//             var button = Instantiate(prefabWaveButton, parentWaveButtons);
+//             button.onClick.AddListener(() => SelectWave(button.transform.GetSiblingIndex()));
+//             var txtWave = button.GetComponentInChildren<TextMeshProUGUI>();
+//             txtWave?.SetText($"Wave {waveInfo.waveIndex}");
+//             btnAddWave.transform.SetAsLastSibling();
+//             SelectWave(button.transform.GetSiblingIndex());
+//         }
+//
+//         public void SelectWave(int waveIndex)
+//         {
+//             if (waveIndex < 0 || waveIndex >= parentWaves.childCount) return;
+//             
+//             // Hide all buttons and waves
+//             foreach (Transform child in parentWaveButtons)
+//             {
+//                 if (child.name == btnAddWave.transform.name) continue;
+//                 var button = child.GetComponent<Button>();
+//                 button.targetGraphic.color = Color.white;
+//             }
+//             foreach (Transform child in parentWaves)
+//             {
+//                 child.gameObject.SetActive(false);
+//                 if (child.TryGetComponent<EndlessWaveEditor>(out var wave))
+//                 {
+//                     wave.Selecting = false;
+//                 }
+//             }
+//
+//             var selectButton = parentWaveButtons.GetChild(waveIndex).GetComponent<Button>();
+//             var selectWave = parentWaves.GetChild(waveIndex);
+//             selectButton.targetGraphic.color = Color.cyan;
+//             selectWave.gameObject.SetActive(true);
+//             if (selectWave.TryGetComponent<EndlessWaveEditor>(out var selectWaveScript))
+//             {
+//                 selectWaveScript.Selecting = true;
+//             }
+//         }
+//
+//         public void DeleteSelectingWave()
+//         {
+//             popupConfirm.Setup(
+//                 "Remember to save your changes!",
+//                 "Confirm delete wave?",
+//                 () =>
+//                 {
+//                     foreach (Transform child in parentWaves)
+//                     {
+//                         if (child.TryGetComponent<EndlessWaveEditor>(out var wave))
+//                         {
+//                             if (wave.Selecting)
+//                             {
+//                                 DeleteWave(child.GetSiblingIndex());
+//                             }
+//                         }
+//                     }
+//                     
+//                     LoadLevel(currentLevel.level);
+//                     popupConfirm.gameObject.SetActive(false);
+//                 }, () =>
+//                 {
+//                     popupConfirm.gameObject.SetActive(false);
+//                 });
+//             popupConfirm.gameObject.SetActive(true);
+//         }
+//         
+//         public void DeleteWave(int waveIndex)
+//         {
+//             if (!currentLevel) return;
+//             if (waveIndex < 0 || waveIndex >= parentWaves.childCount) return;
+//             
+//             var newWaveInfos = new List<WaveInfo>();
+//             WaveConfig configToDelete = null;
+//             foreach (var waveInfo in currentLevel.waveInfo)
+//             {
+//                 if (waveInfo.waveIndex != waveIndex)
+//                 {
+//                     newWaveInfos.Add(waveInfo);
+//                 }
+//                 else
+//                 {
+//                     configToDelete = waveInfo.waveConfig;
+//                 }
+//             }
+//             currentLevel.waveInfo = newWaveInfos.ToArray();
+//             
+// #if UNITY_EDITOR
+//             if (configToDelete)
+//             {
+//                 var wavePath = AssetDatabase.GetAssetPath(configToDelete);
+//                 AssetDatabase.DeleteAsset(wavePath);
+//             }
+//
+//             for (var i = 0; i < currentLevel.waveInfo.Length; i++)
+//             {
+//                 currentLevel.waveInfo[i].waveIndex = i;
+//                 var configPath = AssetDatabase.GetAssetPath(currentLevel.waveInfo[i].waveConfig);
+//                 AssetDatabase.RenameAsset(configPath, $"Level_{currentLevel.level}_Wave_{i + 1}");
+//             }
+//             EditorUtility.SetDirty(currentLevel);
+//             AssetDatabase.SaveAssets();
+//             AssetDatabase.Refresh();
+// #endif
+//         }
+//
+//         #endregion
+//
+//         #region Save
+//
+//         public void SaveLevel()
+//         {
+//             if (!currentLevel) return;
+//             foreach (Transform child in parentWaves)
+//             {
+//                 if (child.TryGetComponent<EndlessWaveEditor>(out var wave))
+//                 {
+//                     wave.SaveWave();
+//                 }
+//             }
+//
+//             currentLevel.mapType = currentMapType;
+//             currentLevel.towerPositions = LevelTowerEditorV2.Instance.GetPositions(currentMapType);
+//             currentLevel.backgroundIndex = currentBgIndex;
+//                         
+// #if UNITY_EDITOR
+//             EditorUtility.SetDirty(currentLevel);
+//             AssetDatabase.SaveAssets();
+//             AssetDatabase.Refresh();
+// #endif
+//         }
+//
+//         #endregion
+// #endif
+//     }
+// }
---
+++ Assets/Dark/Scripts/InGame/Level/EndlessEditor/EndlessWaveEditor.cs
@@ -0,0 +1,329 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using InGame.EndlessLevel;
+using InGame.GateEditorV2;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InGame.EndlessEditor
+{
+    public class EndlessWaveEditor : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        public Transform parentGates;
+        public LevelGatePrefabEditorV2 prefabGate;
+        public Transform parentGateConfig;
+        public LevelGateConfigEditorV2 prefabGateConfig;
+        public Button btnDeleteGate;
+        public Button btnAddGate;
+        public TMP_Dropdown drdGateType;
+        public Button btnEditGateSpawnPosition;
+        public LevelGateSpawnPositionEditorV2 spawnPositionEditor;
+        public TMP_Dropdown drdMapType;
+        public TMP_Dropdown drdBgIndex;
+        public Button btnEditTower;
+        public LevelTowerPositionEditorV2 towerPositionEditor;
+        
+        [FolderOnly] public DefaultAsset configFolderPath;
+
+        public List<EnemyBehaviour> availableEnemies;
+        public Dictionary<int, GateEntity> gateMap;
+
+        public WaveEndlessConfig waveConfig;
+        private Transform[] targetPositions;
+        public bool Selecting { get; set; }
+        public int currentGateType;
+        public int currentMapType;
+        public int currentBgIndex;
+
+        private List<EnemyBehaviour> AvailableEnemies
+        {
+            get
+            {
+                if (availableEnemies == null)
+                {
+                    availableEnemies = new List<EnemyBehaviour>();
+                    GetAvailableEnemies();
+                }
+                return availableEnemies;
+            }    
+        }
+
+        [Button]
+        public void GetAvailableEnemies()
+        {
+            var folderPath = UnityEditor.AssetDatabase.GetAssetPath(configFolderPath);
+            availableEnemies = AssetUtility.LoadAllScriptableObjectsInFolder<EnemyBehaviour>(folderPath).ToList();
+            availableEnemies.Sort((enemy1, enemy2) => enemy1.enemyId.CompareTo(enemy2.enemyId));
+        }
+
+        private void Awake()
+        {
+            if (Camera.main)
+                targetPositions = FindObjectsByType<TowerEntity>(FindObjectsInactive.Include, FindObjectsSortMode.None).Select((tower) => tower.transform).ToArray();
+        }
+
+        private void Start()
+        {
+            _ = AvailableEnemies;
+            
+            btnDeleteGate.onClick.RemoveAllListeners();
+            btnDeleteGate.onClick.AddListener(DeleteSelectingGate);
+            
+            btnAddGate.onClick.RemoveAllListeners();
+            btnAddGate.onClick.AddListener(() => AddGate(
+                new GateConfig() { targetBaseIndex = Array.Empty<int>(), spawnType = AvailableEnemies[0], spawnLogic = new GateSpawnSingle() },
+                currentGateType));
+            
+            btnEditGateSpawnPosition.onClick.RemoveAllListeners();
+            btnEditGateSpawnPosition.onClick.AddListener(OpenSpawnPositionEditor);
+            
+            drdGateType.ClearOptions();
+            drdGateType.options = gateMap.Select(pair => 
+                new TMP_Dropdown.OptionData($"{pair.Key} - {pair.Value.name}")).ToList();
+            drdGateType.onValueChanged.RemoveAllListeners();
+            drdGateType.onValueChanged.AddListener((value) => currentGateType = value);
+            
+            drdMapType.ClearOptions();
+            var options = new List<TMP_Dropdown.OptionData>();
+            for (var i = 0; i < Enum.GetValues(typeof(LevelMapType)).Length; i++)
+            {
+                options.Add(new TMP_Dropdown.OptionData($"{(LevelMapType)i}"));
+            }
+            drdMapType.options = options;
+            drdMapType.onValueChanged.RemoveAllListeners();
+            drdMapType.onValueChanged.AddListener((value) => currentMapType = value);
+            
+            drdBgIndex.ClearOptions();
+            drdBgIndex.options = new List<int>() { 1, 2, 3 }.Select(index => 
+                new TMP_Dropdown.OptionData($"Bg {index}")).ToList();
+            drdBgIndex.onValueChanged.RemoveAllListeners();
+            drdBgIndex.onValueChanged.AddListener((value) => currentBgIndex = value);
+            
+            btnEditTower.onClick.RemoveAllListeners();
+            btnEditTower.onClick.AddListener(() =>
+            {
+                if (!waveConfig) return;
+                towerPositionEditor.Setup(LevelTowerEditorV2.Instance.GetTowers((LevelMapType)currentMapType));
+                towerPositionEditor.gameObject.SetActive(true);
+            });
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Delete))
+            {
+                DeleteSelectingGate();
+            }
+        }
+
+        public void UpdateUI(WaveEndlessConfig waveInfo)
+        {
+            gateMap ??= GateManifest.GetAll();
+            
+            waveConfig = waveInfo;
+            if (!waveInfo) return;
+            
+            LevelTowerEditorV2.Instance.SetPosition((LevelMapType)currentMapType, waveConfig.towerPositions);
+            
+            // Delete all gate
+            foreach (Transform child in parentGates)
+                Destroy(child.gameObject);
+            
+            if (waveInfo.gateConfigs == null) return;
+            foreach (var gateConfig in waveInfo.gateConfigs)
+            {
+                AddGate(gateConfig, GetGatePrefabId(gateConfig.gatePrefab));
+            }
+        }
+
+        public int GetGatePrefabId(GateEntity gatePrefab)
+        {
+            if (gateMap == null) return 0;
+            if (!gatePrefab) return 0;
+            
+            return gateMap.FirstOrDefault((pair) => pair.Value == gatePrefab).Key;
+        }
+        
+        public void AddGate(GateConfig gateConfig, int gatePrefabId)
+        {
+            var newGate = Instantiate(prefabGate, parentGates);
+            newGate.UpdateUI(gateConfig, gatePrefabId);
+            newGate.TargetPositions = targetPositions;
+            newGate.OnClick = SelectGate;
+            newGate.OnDragging = null;
+            var newGateConfig = Instantiate(prefabGateConfig, parentGateConfig);
+            newGateConfig.AvailableEnemies = new Dictionary<int, EnemyBehaviour>();
+            foreach (var enemy in AvailableEnemies)
+            {
+                newGateConfig.AvailableEnemies[enemy.enemyId] = enemy;
+            }
+            newGateConfig.GateMap = gateMap;
+            newGateConfig.Setup(newGate);
+        }
+        
+        public void SelectGate(LevelGatePrefabEditorV2 gate)
+        {
+            foreach (Transform child in parentGates)
+            {
+                var childScript = child.GetComponent<LevelGatePrefabEditorV2>();
+                if (!childScript) continue;
+                childScript.DeselectGate();
+            }
+
+            foreach (Transform child in parentGateConfig)
+            {
+                if (child.TryGetComponent<LevelGateConfigEditorV2>(out var childGateConfig))
+                {
+                    if (childGateConfig.targetGate == gate)
+                        childGateConfig.Select();
+                    else
+                        childGateConfig.Deselect();
+                }
+            }
+            
+            gate.SelectGate();
+        }
+
+        private void OpenSpawnPositionEditor()
+        {
+            foreach (Transform child in parentGateConfig)
+            {
+                if (child.TryGetComponent<LevelGateConfigEditorV2>(out var childGateConfig))
+                {
+                    if (childGateConfig.targetGate.Selecting)
+                    {
+                        spawnPositionEditor.Setup(childGateConfig.targetGate);
+                        return;
+                    }
+                }
+            }
+        }
+        
+        private void DeleteSelectingGate()
+        {
+            foreach (Transform child in parentGateConfig)
+            {
+                if (child.TryGetComponent<LevelGateConfigEditorV2>(out var childGateConfig))
+                {
+                    if (childGateConfig.targetGate.Selecting)
+                    {
+                        DeleteGate(childGateConfig.targetGate);
+                        Destroy(childGateConfig.gameObject);
+                    }
+                }
+            }
+        }
+        
+        public void DeleteGate(LevelGatePrefabEditorV2 gate)
+        {
+            Destroy(gate.gameObject);
+        }
+        
+        #region Save
+
+        public void SaveWave()
+        {
+            if (!waveConfig) return;
+
+            waveConfig.mapType = (LevelMapType)currentMapType;
+            waveConfig.backgroundIndex = currentBgIndex;
+            waveConfig.towerPositions = LevelTowerEditorV2.Instance.GetPositions((LevelMapType)currentMapType);
+            
+            waveConfig.gateConfigs = new List<GateConfig>();
+            foreach (Transform gate in parentGates)
+            {
+                if (gate.TryGetComponent<LevelGatePrefabEditorV2>(out var gateConfigEditor))
+                {
+                    gateConfigEditor.Config.position = gateConfigEditor.Position;
+                    
+                    var newTargets = new List<int>();
+                    if (gateConfigEditor.Config.targetBaseIndex != null)
+                    {
+                        newTargets.AddRange(gateConfigEditor.Config.targetBaseIndex);
+                    }
+
+                    IGateSpawner newSpawnLogic;
+                    if (gateConfigEditor.Config.spawnLogic is GateSpawnSingle existingSingle)
+                    {
+                        newSpawnLogic = new GateSpawnSingle() { radius = existingSingle.radius, randomSpanAngle = existingSingle.randomSpanAngle };
+                    }
+                    else if (gateConfigEditor.Config.spawnLogic is GateSpawnTriangle existingTriangle)
+                    {
+                        newSpawnLogic = new GateSpawnTriangle() {  radius = existingTriangle.radius, randomSpanAngle = existingTriangle.randomSpanAngle };
+                    }
+                    else if (gateConfigEditor.Config.spawnLogic is GateSpawnMultiple existingMultiple)
+                    {
+                        newSpawnLogic = new GateSpawnMultiple() { amount = existingMultiple.amount, randomSpanAngle = existingMultiple.randomSpanAngle, maxRadius = existingMultiple.maxRadius };
+                    }
+                    else if (gateConfigEditor.Config.spawnLogic is GateSpawnPositions existingPositions)
+                    {
+                        newSpawnLogic = new GateSpawnPositions() { amount = existingPositions.amount };
+                
+                        if (gateConfigEditor.SpawnPositions != null)
+                        {
+                            var newSpawnPositions = new List<Vector2>();
+                            var newSpawnPositionInfos = new List<GateSpawnPositionInfo>();
+                            
+                            foreach (var position in gateConfigEditor.SpawnPositions)
+                            {
+                                newSpawnPositions.Add(position.Value.spawnPosition);
+                                var attackPos = new List<Vector2>();
+                                if (position.Value.attackPositions != null)
+                                {
+                                    foreach (var pos in position.Value.attackPositions)
+                                    {
+                                        attackPos.Add(pos);
+                                    }
+                                }
+                                newSpawnPositionInfos.Add(new GateSpawnPositionInfo()
+                                {
+                                    spawnPosition = position.Value.spawnPosition,
+                                    attackPositions = attackPos.ToArray()
+                                });
+                            }
+                    
+                            newSpawnLogic = new GateSpawnPositions()
+                            {
+                                amount = existingPositions.amount, 
+                                spawnPositions = newSpawnPositions.ToArray(),
+                                spawnPositionInfos = newSpawnPositionInfos.ToArray()
+                            };
+                        }
+                    }
+                    else
+                    {
+                        newSpawnLogic = new GateSpawnCenter();
+                    }
+                    
+                    waveConfig.gateConfigs.Add(new GateConfig()
+                    {
+                        isBossGate = gateConfigEditor.Config.isBossGate,
+                        position = gateConfigEditor.Position,
+                        targetBaseIndex = newTargets.ToArray(),
+                        startTime = gateConfigEditor.Config.startTime,
+                        duration = gateConfigEditor.Config.duration,
+                        spawnType = gateConfigEditor.Config.spawnType,
+                        intervalLoop = gateConfigEditor.Config.intervalLoop,
+                        spawnLogic = newSpawnLogic,
+                        startTimeVisual = gateConfigEditor.Config.startTimeVisual,
+                        durationVisual = gateConfigEditor.Config.durationVisual,
+                        gatePrefab = gateConfigEditor.Config.gatePrefab,
+                        hideOrb = gateConfigEditor.Config.hideOrb
+                    });
+                }
+            }
+            
+            EditorUtility.SetDirty(waveConfig);
+            AssetDatabase.SaveAssets();
+        }
+
+        #endregion
+                
+#endif
+    }
+}
